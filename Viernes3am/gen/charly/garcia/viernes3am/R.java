e82a35ba712ff38aa6b3494ea6eef449d5bebc49
lizj <lizj@gmail.com>
2013-05-13 16:42:41 -0400

    fragments implementation

--- Viernes3am/gen/charly/garcia/viernes3am/R.java
+++ Viernes3am/gen/charly/garcia/viernes3am/R.java
@@ -22,6 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon_photos_tab=0x7f020001;
+        public static final int icon_songs_tab=0x7f020002;
+        public static final int icon_videos_tab=0x7f020003;
+        public static final int photos_gray=0x7f020004;
+        public static final int photos_white=0x7f020005;
+        public static final int songs_gray=0x7f020006;
+        public static final int songs_white=0x7f020007;
+        public static final int videos_gray=0x7f020008;
+        public static final int videos_white=0x7f020009;
     }
     public static final class id {
         public static final int group2=0x7f080002;
@@ -32,6 +41,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int photos_layout=0x7f030001;
+        public static final int songs_layout=0x7f030002;
+        public static final int videos_layout=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
