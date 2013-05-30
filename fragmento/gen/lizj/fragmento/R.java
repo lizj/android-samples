0e679eca0281c82ea8991c8c561cf794379a0f24
lizj <lizj@gmail.com>
2013-05-30 13:43:02 -0400

    --

--- fragmento/gen/lizj/fragmento/R.java
+++ fragmento/gen/lizj/fragmento/R.java
@@ -12,6 +12,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_tab_photos_selected=0x7f020001;
+        public static final int ic_tab_photos_unselected=0x7f020002;
+        public static final int ic_tab_videos_selected=0x7f020003;
+        public static final int ic_tab_videos_unselected=0x7f020004;
+        public static final int icon_bag=0x7f020005;
+        public static final int icon_home=0x7f020006;
+        public static final int icon_style=0x7f020007;
     }
     public static final class id {
         public static final int gallery1=0x7f050000;
